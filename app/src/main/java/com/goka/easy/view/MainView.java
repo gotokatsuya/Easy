4a9b823d42f62cebe6b9c3491e91458c66a5c65a
goto <katsuya.goto@eure.jp>
2015-08-21 09:55:33 +0900

    Oops

--- app/src/main/java/com/goka/easy/view/MainView.java
+++ app/src/main/java/com/goka/easy/view/MainView.java
@@ -24,7 +24,7 @@ import javax.inject.Inject;
 public class MainView extends FrameLayout implements ViewImpl<MainActivity> {
 
     @Inject
-    public MainPresenter.Presenter mPresenter;
+    MainPresenter.Presenter mPresenter;
 
     private ListView mArticleListView;
 
